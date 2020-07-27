a31ca2b6c3c762fe74e018623e9c3b7e9c7dc538
kkern42 <kjk9cd@virginia.edu>
2020-07-27 12:58:25 -0400

    firebase it up

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBnB8dZjvXD4Ze8-X7LNspLZC6dIgV5Wzw",
+    authDomain: "school-aa57c.firebaseapp.com",
+    databaseURL: "https://school-aa57c.firebaseio.com",
+    projectId: "school-aa57c",
+    storageBucket: "school-aa57c.appspot.com",
+    messagingSenderId: "552355200282",
+    appId: "1:552355200282:web:116df200afcb10be5980a3"
+};
+firebase.initializeApp(firebaseConfig);
+export default firebase;
